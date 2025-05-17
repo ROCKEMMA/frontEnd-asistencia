1c1d613efff954040aac04ba992af54b370f687e
Santiago <slopez@scl.edu.gt>
2025-05-16 19:29:57 -0600

    avance de asistencia alumno

---
+++ pages/asistencia.js
@@ -0,0 +1,15 @@
+import { moduloAsistencia } from "../modules/asistencia/asistenciaModulo.js";
+
+document.body.appendChild(moduloAsistencia("Nombre Apellido:",));
+document.body.appendChild(moduloAsistencia("Nombre Apellido:",));
+document.body.appendChild(moduloAsistencia("Nombre Apellido:",));
+document.body.appendChild(moduloAsistencia("Nombre Apellido:",));
+document.body.appendChild(moduloAsistencia("Nombre Apellido:",));
+document.body.appendChild(moduloAsistencia("Nombre Apellido:",));
+document.body.appendChild(moduloAsistencia("Nombre Apellido:",));
+document.body.appendChild(moduloAsistencia("Nombre Apellido:",));
+document.body.appendChild(moduloAsistencia("Nombre Apellido:",));
+
+
+
+
