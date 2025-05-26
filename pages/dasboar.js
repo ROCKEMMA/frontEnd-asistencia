eeb2726e5af724fae9a8c2f76c110b090a24c774
Santiago <slopez@scl.edu.gt>
2025-05-25 19:14:58 -0600

    se completa el modulo de tipo de uniforme

--- pages/dasboar.js
+++ pages/dasboar.js
@@ -1,5 +1,7 @@
 import { headerModulo } from "../modules/header/headerModulo.js";
 import { cargarGradosView } from "../views/grados/gradosView.js";
+import { moduloUniforme } from "../modules/uniforme/uniformeModulo.js";
 
 document.body.appendChild(headerModulo("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.TCcYPZkra7mTMPCWre3uagAAAA%26pid%3DApi&f=1&ipt=8f255ef1389d07972910aa50f4f5e2fa0a9cb54dbb3a090d9aa506d50a588d0f","Jossue Fuentes"));
 document.body.appendChild(cargarGradosView("Bachilleratos"));
+document.body.appendChild(moduloUniforme("Tipo de Uniforme"));
