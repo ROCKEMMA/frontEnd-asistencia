6cd0227ea26696fe0d0861ce2e0504561752c850
rockemma <jossuerm55@gmail.com>
2025-06-25 14:00:05 -0600

    Se completa proyecciones de administrador

--- views/proyeccion/proyeccionView.js
+++ views/proyeccion/proyeccionView.js
@@ -1,31 +1,13 @@
 import { cargarCSS } from "../../controles/controlCSS.js";
 import { headerModulo } from "../../modules/header/headerModulo.js";
 import { crearGraficaAsistencia } from "../../modules/nivelGrafico/nivelGraficoModulo.js";
+import { consultarPorcentajePorNivel } from "../../controles/consultarPromedioPorNivel.js";
 
-async function cargarGrafica(seccionProyecciones) {
-  try {
-    const response = await fetch(
-      "http://localhost:3000/porcentajeGrados?nivel_id=3&mes=5&año=2025"
-    );
-    if (!response.ok) throw new Error("Error en la respuesta del servidor");
-
-    const data = await response.json();
-
-    const grafica = await crearGraficaAsistencia(
-      data.nombreNivel,
-      data.porcentajesGrados
-    );
-    seccionProyecciones.appendChild(grafica);
-  } catch (error) {
-    console.log("Error de carga:", error);
-    seccionProyecciones.innerHTML = "<p>Error al cargar los datos</p>";
-  }
-}
-
-export function proyeccionView() {
+export async function proyeccionView() {
   cargarCSS("../views/proyeccion/proyeccionView.css");
 
-  document.body.appendChild(
+  const contenedor = document.createElement("div");
+  contenedor.appendChild(
     headerModulo(
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.TCcYPZkra7mTMPCWre3uagAAAA%26pid%3DApi&f=1&ipt=8f255ef1389d07972910aa50f4f5e2fa0a9cb54dbb3a090d9aa506d50a588d0f",
       "Jossue Fuentes"
@@ -35,9 +17,35 @@ export function proyeccionView() {
   const seccionProyecciones = document.createElement("section");
   seccionProyecciones.className = "seccion-proyecciones";
 
-  cargarGrafica(seccionProyecciones);
+  const configuraciones = [
+    { nivel: 3, mes: 5 },
+    { nivel: 4, mes: 5 },
+    { nivel: 1, mes: 5 },
+  ];
 
-  return seccionProyecciones;
+  try {
+    const datosArray = await Promise.all(
+      configuraciones.map(({ nivel, mes }) =>
+        consultarPorcentajePorNivel(nivel, mes)
+      )
+    );
+
+    for (const datos of datosArray) {
+      const grafica = await crearGraficaAsistencia(
+        datos.nombreNivel,
+        datos.porcentajesGrados
+      );
+      seccionProyecciones.appendChild(grafica);
+    }
+  } catch (error) {
+    console.log("Error al consultar los datos:", error);
+    seccionProyecciones.innerHTML = "<p>Error al cargar los datos</p>";
+  }
+
+  contenedor.appendChild(seccionProyecciones);
+  return contenedor;
 }
 
-document.body.appendChild(proyeccionView());
+proyeccionView().then((vista) => {
+  document.body.appendChild(vista);
+});
