453721d953ac08da07ee4c939db442804e83325b
rockemma <jossuerm55@gmail.com>
2025-07-01 20:31:54 -0600

    abance de configuración

--- views/grados/gradosView.js
+++ views/grados/gradosView.js
@@ -42,7 +42,7 @@ async function cargarGradosView(nivel) {
     return sectionGrados;
 }
 
-document.body.appendChild(headerModulo("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.TCcYPZkra7mTMPCWre3uagAAAA%26pid%3DApi&f=1&ipt=8f255ef1389d07972910aa50f4f5e2fa0a9cb54dbb3a090d9aa506d50a588d0f","Jossue Fuentes"));
+document.body.appendChild(headerModulo());
 
 cargarGradosView("Grados").then(section => {
     document.body.appendChild(section);
